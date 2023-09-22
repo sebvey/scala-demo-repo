efbba62f6e962c73f0fd35ee0a87294826c3a71a
Sebastien VEY <sebvey@gmail.com>
2023-09-22 15:13:08 +0200

    .

--- sbt-spark-base/src/main/scala/edu.sve.ssb/ScalaSparkBaseApp.scala
+++ sbt-spark-base/src/main/scala/edu.sve.ssb/ScalaSparkBaseApp.scala
@@ -2,20 +2,25 @@ package edu.sve.ssb
 
 import org.apache.spark.sql._
 
-object Main {
+object ScalaSparkBaseApp extends App {
 
-    def Main(args: Array[String]): Unit = {
+    override def main(args: Array[String]): Unit = {
 
         val spark = SparkSession.builder()
             .master("local[1]")
             .appName("SbtSparkBase")
-            getOrCreate
+            .getOrCreate
 
         // Remove Spark INFO/WARN logs (after this spark session is created)
         spark.sparkContext.setLogLevel("WARN")
 
         var inPath = this.getClass.getResource("/titanic-dataset.csv").getPath
-        println(inPath)
+
+        val df = spark.read
+            .option("header","true")
+            .csv(inPath)
+
+        df.show(false)
 
     }
 
