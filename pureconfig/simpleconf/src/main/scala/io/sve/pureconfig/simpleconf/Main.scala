3b242abeafa3e0e205ded7ba81aa77ba875c80d9
Sebastien VEY <sebvey@gmail.com>
2024-02-25 01:38:32 +0100

    TODO list

--- pureconfig/simpleconf/src/main/scala/io/sve/pureconfig/simpleconf/Main.scala
+++ pureconfig/simpleconf/src/main/scala/io/sve/pureconfig/simpleconf/Main.scala
@@ -20,4 +20,11 @@ object Main extends App {
     val conf = ConfigSource.default.loadOrThrow[MyConf]
     println(conf)
 
+    // TODO :
+    // trait ConfApp extends App qui permet de lire la conf et la rendre dispo en variable conf
+    // lire la conf non pas dans resources mais dans conf/
+    // tester la conf
+    // faire une Spec qu'on peut étendre pour tester la conf par défaut, puis un fichier donné sur valeur overridée
+    // Plus tard : faire un reader pour un Path hadoop
+
 }
